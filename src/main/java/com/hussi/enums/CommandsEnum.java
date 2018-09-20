fe07b2098d8017d090fd009f1a8c576b53a4b397
ZuhaibAnsari <m.zuhaibansari@gmail.com>
2018-09-20 13:01:04 +0100

    Update CommandsEnum.java

--- src/main/java/com/hussi/enums/CommandsEnum.java
+++ src/main/java/com/hussi/enums/CommandsEnum.java
@@ -10,7 +10,7 @@ public enum CommandsEnum
     REGISTRATION_NUMBERS_FOR_CARS_WITH_COLOUR(  "registration_numbers_for_cars_with_colour"),
     SLOT_NUMBERS_FOR_CARS_WITH_COLOUR("slot_numbers_for_cars_with_colour"),
     SLOT_NUMBER_FOR_REGISTRATION_NUMBER("slot_number_for_registration_number");
-
+//Same here more logical name like command and getCommand
     private String value;
 
     public String getValue() {
